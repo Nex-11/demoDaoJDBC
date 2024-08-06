49a0f22945be9a28b975a87b60e8da2763920cee
Nex-11 <brunodmartinscarvalho@gmail.com>
2024-08-06 23:28:50 +0100

    Find by Department implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -92,6 +95,49 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		// INICIAR VARIAVEIS DE COMUNICAÇAO COM A BD
+				PreparedStatement st = null;
+				ResultSet rs = null;
+				try {
+					// STATEMENT QUE REQUESITA A BD
+					st = conn.prepareStatement("SELECT seller.*, department.Name as DepName FROM coursejdbc.seller INNER JOIN coursejdbc.department on seller.DepartmentId = department.Id WHERE DepartmentID = ? ORDER BY Name");
+					// PASSADO O PARAMETRO ESCOLHIDO "department"
+					st.setInt(1, department.getId());
+					rs = st.executeQuery();
+					
+					List<Seller> list = new ArrayList<Seller>();
+					// CRIADA A HASHMAP VAZIA, SERÁ QUARDADO O DEPARTAMENTO
+					Map<Integer, Department> map = new HashMap<>();
+					
+					//  SE A QUERY NÃO RETORNAR NUNHUM VALOR, NÃO EXISTE SELLER
+					while (rs.next()) {
+						// VAI VERIFICAR SE JA EXISTE UM VALOR DE DEPARTMAMENTO IGUAL, SE HOUVER RETORNA NULL PARA A VARIAVEL TEMPORARIA dep
+						Department dep = map.get(rs.getInt("DepartmentId"));
+						
+						if (dep == null) {
+							dep = instanciateDepartment(rs);
+							map.put(rs.getInt("DepartmentId"), dep);
+						}
+					 
+						Seller obj = instanciateSeller(rs,dep);
+						list.add(obj);
+
+					}
+					return null;
+				}
+				catch (SQLException e) {
+					throw new DbException(e.getMessage());
+				}
+				finally{
+					DB.closeStatement(st);
+					DB.closeResultSet(rs);
+				}
+
 	}
 	
 	
