2fb1de7f2ca0020324655ad31321fd8f93cd085e
Nex-11 <brunodmartinscarvalho@gmail.com>
2024-08-06 22:49:45 +0100

    Find by Id Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-
-		Department obj = new Department (1, "Book");
-
-		Seller seller = new Seller(1, "Bruno", "bruno@carvalho.pt", new Date(),5000.00, obj);
-		
-		
-		System.out.println(obj);
-		System.out.println(seller);
-		
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 		
 	}
 
